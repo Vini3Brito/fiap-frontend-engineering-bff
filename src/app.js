68da8a8d493577bcba94ade013323e547d6cdea9
Jaison <jaisonschmidt@gmail.com>
2025-04-20 22:31:40 -0300

    fix: Configura CORS

--- src/app.js
+++ src/app.js
@@ -3,10 +3,19 @@ require('newrelic');
 
 const express = require('express');
 const rateLimit = require('express-rate-limit'); // Importa o rate limit
+const cors = require('cors');
 const askRoute = require('./routes/ask');
 
 const app = express();
 app.use(express.json());
+
+// Configura o CORS
+const corsOptions = {
+  origin: '*', // Permite todas as origens. Altere para um domínio específico, se necessário.
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+};
+app.use(cors(corsOptions));
 
 // Configura o rate limit
 const limiter = rateLimit({
